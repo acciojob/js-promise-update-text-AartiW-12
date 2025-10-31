d958eb05b273a8301af4a63a377b5f473f62c63b
AartiW-12 <aartiwhaval12@gmail.com>
2025-10-31 10:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+function Hello(){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		},1000)
+	})
+}
+
+let content = Hello().then((data)=>{
+	output.textContent = data;
+})
+.catch((err)=>{
+	output.textContent = err;
+})
